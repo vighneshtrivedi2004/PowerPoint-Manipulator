490f5700925a675bbec59f763aa27d207f2c9fed
vighneshtrivedi2004 <vighneshtrivedi2004@gmail.com>
2019-05-25 16:08:19 +0530

    Added code to add custom table to slide

--- PowerPoint Manipulator/ManipulatorHelper/Manipulator.cs
+++ PowerPoint Manipulator/ManipulatorHelper/Manipulator.cs
@@ -1,5 +1,6 @@
 ﻿using Aspose.Slides;
 using Aspose.Slides.Export;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -46,64 +47,114 @@ namespace ManipulatorHelper
         /// <param name="testData"></param>
         public static void ManipulatePowerPointPresentationWithContent(string presentationWithData_FilePath, TestData testData)
         {
-            string listRespondents = null;
-
-            // Creating a presentation instance
-            using (Presentation presentationWithData = new Presentation(presentationWithData_FilePath))
+            string listRespondents = null;                   
+            try
             {
-                //Add respondent template slide to new presentation
-                ISlide respondentTemplateSlide = presentationWithData.Slides[0];
-                presentationWithData.Slides.AddClone(respondentTemplateSlide);
-
-                ISlide lastRespondentSlide = presentationWithData.Slides.Last();
-
-                int startSlideIndex = lastRespondentSlide.SlideNumber - 1;
-
-                foreach (var name in testData.Respondents)
+                // Creating a presentation instance
+                using (Presentation presentationWithData = new Presentation(presentationWithData_FilePath))
                 {
-                    listRespondents += name + "\n";
-                }
+                    //Add respondent template slide to new presentation
+                    ISlide respondentTemplateSlide = presentationWithData.Slides[0];
+                    presentationWithData.Slides.AddClone(respondentTemplateSlide);
 
-                //Modify newly added respondent template slide with content
-                foreach (IShape shp in lastRespondentSlide.Shapes)
-                {
-                    if (shp != null)
+                    ISlide lastRespondentSlide = presentationWithData.Slides.Last();
+
+                    int startSlideIndex = lastRespondentSlide.SlideNumber - 1;
+
+                    foreach (var name in testData.Respondents)
                     {
-                        var text = ((IAutoShape)shp).TextFrame.Text;
-                        if (text.StartsWith("List"))
+                        listRespondents += name + "\n";
+                    }
+
+                    //Modify newly added respondent template slide with content
+                    foreach (IShape shp in lastRespondentSlide.Shapes)
+                    {
+                        if (shp != null)
                         {
-                            ((IAutoShape)shp).TextFrame.Text = "Respondents with test list:\n" + listRespondents;
+                            var text = ((IAutoShape)shp).TextFrame.Text;
+                            if (text.StartsWith("List"))
+                            {
+                                ((IAutoShape)shp).TextFrame.Text = "Respondents with test list:\n" + listRespondents;
+                            }
                         }
                     }
-                }
 
-                //Modify area template slide for each Slide 
-                ISlide areaTemplateSlide = presentationWithData.Slides[1];
-                foreach (var slide in testData.Slides)
-                {
-                    presentationWithData.Slides.AddClone(areaTemplateSlide);
-
-                    ISlide lastSlide = presentationWithData.Slides.Last();
-
-                    // Iterate through shapes to find the placeholder
-                    foreach (IShape shp in lastSlide.Shapes)
+                    //Modify area template slide for each Slide 
+                    ISlide areaTemplateSlide = presentationWithData.Slides[1];
+                    foreach (var slide in testData.Slides)
                     {
-                        if (shp.Placeholder != null)
+                        List<double> listDblColsWidths = new List<double>();
+                        List<double> listDblRowsHeights = new List<double>();
+                        int rowCount = 0, colCount = 0;
+
+                        presentationWithData.Slides.AddClone(areaTemplateSlide);
+
+                        ISlide lastSlide = presentationWithData.Slides.Last();
+
+                        //Delete all existing tables from area slide
+                        for (int k = 0; k < lastSlide.Shapes.Count(); k++)
                         {
-                            // Change the text of placeholder                           
-                            ((IAutoShape)shp).TextFrame.Text = slide.Header;
+                            var shp = lastSlide.Shapes[k];
+                            if (shp is ITable) //Delete if shape is table
+                            {
+                                lastSlide.Shapes.Remove(shp);
+                                k = k - 1; //Decrement counter as one shape has been deleted
+                            }
+                        }
+
+                        // Iterate through shapes to find the placeholder
+                        foreach (IShape shp in lastSlide.Shapes)
+                        {
+                            if (shp.Placeholder != null)
+                            {
+                                // Change the text of placeholder                           
+                                ((IAutoShape)shp).TextFrame.Text = slide.Header;
+                            }
+                        }
+
+                        // Add Question table to slide
+                        rowCount = slide.Questions.Count() + 1;
+                        colCount = testData.Respondents.Count() + 1;
+
+                        for (int row = 0; row < rowCount; row++)
+                        {
+                            listDblRowsHeights.Add(5);
+                        }
+
+                        listDblColsWidths.Add(385);
+                        for (int col = 1; col < colCount; col++)
+                        {
+                            listDblColsWidths.Add(51);
+                        }
+
+                        double[] dblColsWidths = listDblColsWidths.ToArray();
+                        double[] dblRowsHeights = listDblRowsHeights.ToArray();
+                        ITable questionsTable = lastSlide.Shapes.AddTable(56, 100, dblColsWidths, dblRowsHeights);
+
+                        for (int i = 0; i < questionsTable.Rows.Count - 1; i++)
+                        {
+                            questionsTable[0, i + 1].TextFrame.Text = slide.Questions[i];
+                            for (int j = 0; j < questionsTable.Columns.Count - 1; j++)
+                            {
+                                questionsTable[j + 1, 0].TextFrame.Text = testData.Respondents[j];
+                            }
+
                         }
                     }
-                }
 
-                //Delete all template slides
-                for (int i = startSlideIndex - 1; i >= 0; i--)
-                {
-                    presentationWithData.Slides.RemoveAt(i);
-                }
+                    //Delete all template slides
+                    for (int i = startSlideIndex - 1; i >= 0; i--)
+                    {
+                        presentationWithData.Slides.RemoveAt(i);
+                    }
 
-                //Writing the presentation as a PPTX file
-                presentationWithData.Save(presentationWithData_FilePath, SaveFormat.Pptx);
+                    //Writing the presentation as a PPTX file
+                    presentationWithData.Save(presentationWithData_FilePath, SaveFormat.Pptx);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
             }
         }
     }
